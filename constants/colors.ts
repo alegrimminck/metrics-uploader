914d3338a1b8e4f14dddec524df26d85635fcd9d
Alejandro Grimminck <alegrimminck@gmail.com>
2024-01-07 22:43:46 -0300

    feat(constant): add color constants

---
+++ constants/colors.ts
@@ -0,0 +1,15 @@
+interface Colors {
+    primary: string
+    "gray-1": string
+    "gray-2": string
+    "gray-3": string
+}
+
+const colors: Colors = {
+    primary: '#5DB075',
+    "gray-1": '#F7F7F7',
+    "gray-2": '#E1E1E1',
+    "gray-3": '#BDBDBD'
+}
+
+export default colors;

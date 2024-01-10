e6b439e10ec1d87eb1a1300d2d1998b2414bca42
Alejandro Grimminck <alegrimminck@gmail.com>
2024-01-10 20:22:55 -0300

    feat(colors): add secondary color

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,5 +1,6 @@
 interface Colors {
     primary: string
+    secondary: string
     "gray-1": string
     "gray-2": string
     "gray-3": string
@@ -7,6 +8,7 @@ interface Colors {
 
 const colors: Colors = {
     primary: '#5DB075',
+    secondary: 'blue',
     "gray-1": '#F7F7F7',
     "gray-2": '#E1E1E1',
     "gray-3": '#BDBDBD'
